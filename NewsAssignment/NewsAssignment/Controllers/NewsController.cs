4ff4b2093b5cd7a0eb1635d6d087e11abc65ff03
tahsinur <tahsinur777@gmail.com>
2022-04-05 04:05:33 +0600

    news edit,delete done in controller in presentation layer

--- NewsAssignment/NewsAssignment/Controllers/NewsController.cs
+++ NewsAssignment/NewsAssignment/Controllers/NewsController.cs
@@ -40,5 +40,28 @@ namespace NewsAssignment.Controllers
             var list = NewsService.Get(id);
             return Request.CreateResponse(HttpStatusCode.OK, list);
         }
+
+        [Route("api/News/Edit")]
+        [HttpPost]
+        public HttpResponseMessage Edit(NewsModel news)
+        {
+            if (ModelState.IsValid)
+            {
+                var flag = NewsService.Edit(news);
+                if (flag) return Request.CreateResponse(HttpStatusCode.OK, "Edited");
+                else return Request.CreateResponse(HttpStatusCode.OK, "Not Edited");
+            }
+            return Request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+        }
+
+        [Route("api/News/Delete/{id}")]
+        [HttpDelete]
+        public HttpResponseMessage Delete(int id)
+        {
+            var flag = NewsService.Delete(id);
+            if (flag) return Request.CreateResponse(HttpStatusCode.OK, "Deleted");
+            else return Request.CreateResponse(HttpStatusCode.OK, "Not Delete");
+        }
+
     }
 }
