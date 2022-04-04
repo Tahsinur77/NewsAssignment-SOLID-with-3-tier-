da17891a9717f4806a3793ff42c0783b43cf5f2f
tahsinur <tahsinur777@gmail.com>
2022-04-05 02:38:53 +0600

    In newsController add, list, search by id done

---
+++ NewsAssignment/NewsAssignment/Controllers/NewsController.cs
@@ -0,0 +1,44 @@
+﻿using BLL.Entites;
+using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NewsAssignment.Controllers
+{
+    public class NewsController : ApiController
+    {
+        [Route("api/News/Add")]
+        [HttpPost]
+        public HttpResponseMessage Add(NewsModel news)
+        {
+            if (ModelState.IsValid)
+            {
+               var flag =  NewsService.Add(news);
+
+                if (flag) return Request.CreateResponse(HttpStatusCode.OK, "Added");
+                else return Request.CreateResponse(HttpStatusCode.OK, "Not Added");
+            }
+            return Request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+        }
+
+        [Route("api/News/list")]
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            var list = NewsService.Get();
+            return Request.CreateResponse(HttpStatusCode.OK, list);
+        }
+
+        [Route("api/News/{id}")]
+        [HttpGet]
+        public HttpResponseMessage Get(int id)
+        {
+            var list = NewsService.Get(id);
+            return Request.CreateResponse(HttpStatusCode.OK, list);
+        }
+    }
+}
